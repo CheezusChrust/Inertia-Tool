e5dee85c4554c1099c392959d53282a730c8cf33
CheezusChrust <rcheeze1337@gmail.com>
2021-02-28 14:15:41 -0500

    Presets, inertia lock, dupe fixes, HUD update
    - Added preset menu
    - Added inertia lock mode to keep inertia the same regardless of if mass changes
    - Fixed inertia changing unexpectedly after setting inertia, increasing weight without inertia lock, and duping then re-spawning
    - HUD now displays multiplier instead of actual inertia value when set to "Multiple of Default" mode

--- lua/weapons/gmod_tool/stools/inertia.lua
+++ lua/weapons/gmod_tool/stools/inertia.lua
@@ -3,11 +3,12 @@ TOOL.Name = "#Inertia"
 TOOL.Command = nil
 TOOL.ConfigName = nil
 TOOL.ClientConVar["mode"] = "direct"
-TOOL.ClientConVar["x"] = "10"
-TOOL.ClientConVar["y"] = "10"
-TOOL.ClientConVar["z"] = "10"
-TOOL.ClientConVar["xyz"] = "10"
+TOOL.ClientConVar["x"] = "1"
+TOOL.ClientConVar["y"] = "1"
+TOOL.ClientConVar["z"] = "1"
+TOOL.ClientConVar["xyz"] = "1"
 TOOL.ClientConVar["tooltipscale"] = "1"
+TOOL.ClientConVar["lock"] = "0"
 
 if CLIENT then
     TOOL.Information = {
@@ -34,21 +35,24 @@ if CLIENT then
     language.Add("tool.inertia.y", "Y axis inertia")
     language.Add("tool.inertia.z", "Z axis inertia")
     language.Add("tool.inertia.xyz", "XYZ (all axes) inertia")
+    language.Add("tool.inertia.inertialock", "Inertia lock")
+    language.Add("tool.inertia.inertialock.help", "Keep set inertia the same even if the entity's mass changes")
     language.Add("tool.inertia.tooltipscale", "Tooltip Scale")
     language.Add("tool.inertia.warning", "WARNING: VERY HIGH VALUES ON ONE AXIS MAY CAUSE UNEXPECTED RESULTS!\nThis includes things such as props deleting themselves due to crazy physics. Be careful!")
 end
 
 local excluded = {"func_", "prop_dynamic", "prop_ragdoll", "prop_vehicle", "player", "npc"}
-local entityMeta = FindMetaTable("Entity")
+
+local ENTITY = FindMetaTable("Entity")
 
 --Regular vectors are heavily compressed, have to do this for accuracy
-function entityMeta:SetNWVectorPrecise(key, value)
+function ENTITY:SetNWVectorPrecise(key, value)
     self:SetNWFloat(key .. "_x", value[1])
     self:SetNWFloat(key .. "_y", value[2])
     self:SetNWFloat(key .. "_z", value[3])
 end
 
-function entityMeta:GetNWVectorPrecise(key, fallback)
+function ENTITY:GetNWVectorPrecise(key, fallback)
     local x = self:GetNWFloat(key .. "_x", fallback[1])
     local y = self:GetNWFloat(key .. "_y", fallback[2])
     local z = self:GetNWFloat(key .. "_z", fallback[3])
@@ -56,13 +60,15 @@ function entityMeta:GetNWVectorPrecise(key, fallback)
     return Vector(x, y, z)
 end
 
-if entityMeta.OldPhysicsInitSphere then
-    entityMeta.PhysicsInitSphere = entityMeta.OldPhysicsInitSphere
+--Reload support
+if ENTITY.OldPhysicsInitSphere then
+    ENTITY.PhysicsInitSphere = ENTITY.OldPhysicsInitSphere
 end
 
-entityMeta.OldPhysicsInitSphere = entityMeta.PhysicsInitSphere
+ENTITY.OldPhysicsInitSphere = ENTITY.PhysicsInitSphere
 
-function entityMeta:PhysicsInitSphere(...)
+--Making an entity spherical changes its default inertia, so we have to save it again
+function ENTITY:PhysicsInitSphere(...)
     local ret = self:OldPhysicsInitSphere(...)
 
     timer.Simple(0, function()
@@ -118,6 +124,27 @@ local function setInertia(player, entity, data)
 end
 
 duplicator.RegisterEntityModifier("inertia", setInertia)
+local PHYSOBJ = FindMetaTable("PhysObj")
+
+--Reload support
+if PHYSOBJ.OldSetMass then
+    PHYSOBJ.SetMass = PHYSOBJ.OldSetMass
+end
+
+PHYSOBJ.OldSetMass = PHYSOBJ.SetMass
+
+function PHYSOBJ:SetMass(...)
+    self:OldSetMass(...)
+
+    if self:GetEntity().inertiaLock then
+        self:SetInertia(self:GetEntity().inertiaLock)
+    else
+        setInertia(nil, self:GetEntity(), {
+            inertia = self:GetInertia(),
+            defaultInertia = self:GetEntity().defaultInertia
+        })
+    end
+end
 
 function TOOL:Think()
     if CLIENT then return end
@@ -133,17 +160,28 @@ function TOOL:LeftClick(trace)
 
     if SERVER then
         local inertia = Vector(self:GetClientInfo("x"), self:GetClientInfo("y"), self:GetClientInfo("z"))
+        local ent = trace.Entity
 
         if self:GetClientInfo("mode") == "direct" then
-            setInertia(self:GetOwner(), trace.Entity, {
-                inertia = vClamp(inertia, 0.001, 100000000),
-                defaultInertia = trace.Entity.defaultInertia
+            inertia = vClamp(inertia, 0.001, 100000000)
+
+            setInertia(nil, ent, {
+                inertia = inertia,
+                defaultInertia = ent.defaultInertia
             })
         else
-            setInertia(self:GetOwner(), trace.Entity, {
-                inertia = vClamp(trace.Entity.defaultInertia * inertia * trace.Entity:GetPhysicsObject():GetMass(), 0.001, 100000000),
-                defaultInertia = trace.Entity.defaultInertia
+            inertia = vClamp(ent.defaultInertia * inertia * ent:GetPhysicsObject():GetMass(), 0.001, 100000000)
+
+            setInertia(nil, ent, {
+                inertia = inertia,
+                defaultInertia = ent.defaultInertia
             })
+        end
+
+        if self:GetClientInfo("lock") == "1" then
+            ent.inertiaLock = inertia
+        else
+            ent.inertiaLock = nil
         end
     end
 
@@ -175,16 +213,28 @@ function TOOL:Reload(trace)
 
     if SERVER and trace.Entity.defaultInertia then
         trace.Entity:GetPhysicsObject():SetInertia(trace.Entity.defaultInertia * trace.Entity:GetPhysicsObject():GetMass())
+        trace.Entity.inertiaLock = nil
         duplicator.ClearEntityModifier(trace.Entity, "inertia")
     end
 
     return true
 end
 
+local conVarsDefault = TOOL:BuildConVarList()
+
 function TOOL.BuildCPanel(cp)
     cp:AddControl("Header", {
         Text = "#tool.inertia.name",
         Description = "#tool.inertia.desc"
+    })
+
+    cp:AddControl("ComboBox", {
+        MenuButton = 1,
+        Folder = "inertia",
+        Options = {
+            ["#preset.default"] = conVarsDefault
+        },
+        CVars = table.GetKeys(conVarsDefault)
     })
 
     cp:AddControl("ComboBox", {
@@ -239,6 +289,12 @@ function TOOL.BuildCPanel(cp)
     cp:ControlHelp("")
     cp:ControlHelp("#tool.inertia.warning")
 
+    cp:AddControl("toggle", {
+        Label = "#tool.inertia.inertialock",
+        Command = "inertia_lock",
+        Help = true
+    })
+
     cp:AddControl("Slider", {
         Label = "#tool.inertia.tooltipscale",
         Type = "Float",
@@ -277,7 +333,26 @@ if CLIENT then
         obb:Rotate(ent:GetAngles())
         local pos = (ent:GetPos() + obb):ToScreen()
         surface.SetFont("InertiaFont")
-        local str = "Current: " .. matrixToString(ply:GetNWVectorPrecise("inertia", Vector()))
+        local str
+
+        if GetConVar("inertia_mode"):GetString() == "multiplier" then
+            local v1 = ply:GetNWVectorPrecise("inertia", Vector())
+            local v2 = ent:GetNWVectorPrecise("defaultinertia", Vector()) * ply:GetNWFloat("inertiamass")
+            if v1:Length() > 4294967295 then
+                v1 = Vector()
+            end
+            if v2:Length() > 4294967295 then
+                v2 = Vector()
+            end
+            str = "Current: [" .. math.Round(v1.x / v2.x, 2) .. "x, " .. math.Round(v1.y / v2.y, 2) .. "x, " .. math.Round(v1.z / v2.z, 2) .. "x]"
+        else
+            local v = ply:GetNWVectorPrecise("inertia", Vector())
+            if v:Length() > 4294967295 then
+                v = Vector()
+            end
+            str = "Current: " .. matrixToString(v)
+        end
+
         local str2 = "Default: " .. matrixToString(ent:GetNWVectorPrecise("defaultinertia", Vector()) * ply:GetNWFloat("inertiamass"))
         local w, h = surface.GetTextSize(str)
         local w2, _ = surface.GetTextSize(str2)
@@ -286,28 +361,25 @@ if CLIENT then
         mat:Translate(Vector(pos.x, pos.y))
         mat:Scale(Vector(1, 1, 1) * ply:GetInfoNum("inertia_tooltipscale", 1) / 2)
         mat:Translate(-Vector(pos.x, pos.y))
-
         cam.Start3D()
-            render.SetMaterial(Material("cable/red"))
-            render.DrawBeam(ent:GetPos(), ent:LocalToWorld(Vector(25, 0, 0)), 0.5, 0, 1)
-            render.SetMaterial(Material("cable/green"))
-            render.DrawBeam(ent:GetPos(), ent:LocalToWorld(Vector(0, 25, 0)), 0.5, 0, 1)
-            render.SetMaterial(Material("cable/blue"))
-            render.DrawBeam(ent:GetPos(), ent:LocalToWorld(Vector(0, 0, 25)), 0.5, 0, 1)
+        render.SetMaterial(Material("cable/red"))
+        render.DrawBeam(ent:GetPos(), ent:LocalToWorld(Vector(25, 0, 0)), 0.5, 0, 1)
+        render.SetMaterial(Material("cable/green"))
+        render.DrawBeam(ent:GetPos(), ent:LocalToWorld(Vector(0, 25, 0)), 0.5, 0, 1)
+        render.SetMaterial(Material("cable/blue"))
+        render.DrawBeam(ent:GetPos(), ent:LocalToWorld(Vector(0, 0, 25)), 0.5, 0, 1)
         cam.End3D()
-
         local x = ent:LocalToWorld(Vector(27, 0, 0)):ToScreen()
         local y = ent:LocalToWorld(Vector(0, 27, 0)):ToScreen()
         local z = ent:LocalToWorld(Vector(0, 0, 27)):ToScreen()
         draw.SimpleTextOutlined("X", "DermaLarge", x.x, x.y, Color(255, 0, 0), 1, 1, 1, Color(0, 0, 0))
         draw.SimpleTextOutlined("Y", "DermaLarge", y.x, y.y, Color(0, 255, 0), 1, 1, 1, Color(0, 0, 0))
         draw.SimpleTextOutlined("Z", "DermaLarge", z.x, z.y, Color(0, 0, 255), 1, 1, 1, Color(0, 0, 0))
-
         cam.PushModelMatrix(mat)
-            surface.DrawRect(pos.x - w / 2, pos.y - h, w, h)
-            draw.DrawText(str, "InertiaFont", pos.x, pos.y - h, Color(255, 255, 255), 1)
-            surface.DrawRect(pos.x - w2 / 2, pos.y, w2, h)
-            draw.DrawText(str2, "InertiaFont", pos.x, pos.y, Color(255, 255, 255), 1)
+        surface.DrawRect(pos.x - w / 2, pos.y - h, w, h)
+        draw.DrawText(str, "InertiaFont", pos.x, pos.y - h, Color(255, 255, 255), 1)
+        surface.DrawRect(pos.x - w2 / 2, pos.y, w2, h)
+        draw.DrawText(str2, "InertiaFont", pos.x, pos.y, Color(255, 255, 255), 1)
         cam.PopModelMatrix()
     end)
 end
